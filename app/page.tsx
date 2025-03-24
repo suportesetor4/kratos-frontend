93a4e5229252ae1b261e22954411ee1cfc08da6e
suportesetor4 <suporte.setor4@ct.ufrn.br>
2025-03-24 16:29:57 -0300

    setup

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useEffect } from "react";
+import Login from "./login/page";
+import { useRouter } from "next/navigation";
+
+const isLogged = true
+
+export default function Home() {
+
+  const router = useRouter()
+
+  async function isClientLoggedIn(){
+    // verify if user has token in localstorage
+    if(isLogged){
+      router.push("login")
+    } 
+  }
+
+  useEffect(()=>{
+    isClientLoggedIn()
+  }, [])
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
