40985cc05b40655fbe062056ce14da722b49b338
suportesetor4 <suporte.setor4@ct.ufrn.br>
2025-04-01 15:24:35 -0300

    fix: ajeitando erros de build

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
 
-import localFont from 'next/font/local'
 import { AppSidebar } from "@/components/ui/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-
-// Font files can be colocated inside of `pages`
-const gothamXLight = localFont({ src: '../../fonts/gothamXLight.otf' })
 
 export const metadata: Metadata = {
   title: "Kratos - Home",
