0f2867a008b591bcb190c7cfe323a7fc26a9917a
suportesetor4 <suporte.setor4@ct.ufrn.br>
2025-04-09 12:41:41 -0300

    fix: mixed content

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,25 @@
 import type { NextConfig } from "next";
 
+// desativar o async headers na produção real. estamos utilizando isso pelo erro de mixed content entre o ambiente de produção next e desenvolvimento backend
+
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers(){
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requsts'
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
