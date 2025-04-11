654480986a2badccc4cc3b507193f1356525f6c0
suportesetor4 <suporte.setor4@ct.ufrn.br>
2025-04-11 14:26:39 -0300

    feat: add mapa funcional

--- app/home/mapa-setor-4/page.tsx
+++ app/home/mapa-setor-4/page.tsx
@@ -1,131 +1,406 @@
 "use client"
 
+import { OctagonX } from "lucide-react";
+
+type Role = "ADMIN" | "GERAL"
+const role_user : Role = "GERAL"
+
 interface Sala {
     nome: string;
-    status: "OK" | "PENDENTE" | "ANDAMENTO";
-    role: "SUPORTE" | "ADMIN"
+    status: "OK" | "PENDENTE" | "ANDAMENTO" | "INTERDITADO";
+    role: Role;
+    tipo: "LAB" | "SALA" | "AUD" | "BANHEIRO" | "OUTRO";
 }
 
 interface Bloco {
     bloco: ("A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I")
     sentido: "norte" | "sul";
-    salas: string[];
+    salas: Sala[];
 }
 
-const mapa : Bloco[] = [
+let mapa : Bloco[] = [
     {
         bloco: "A",
         sentido: "sul",
         salas: [
-            "A1",
-            "A2",
-            "A3",
-            "A4",
-            "A5",
-
+            {
+                nome: "A1",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "A2",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "A3",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "A4",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "A5",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
         ]
     },
     {
         bloco: "A",
         sentido: "sul",
         salas: [
-            "E. L. & Q.",
-            "A6",
-            "A7",
-            "A8",
-            "A9",
-            "A10"
+            {
+                nome: "E. L. & Q.",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "SALA"
+            },
+            {
+                nome: "A6",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "A7",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "A8",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "A9",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "A10",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
         ]
     },
     {
         bloco: "B",
         sentido: "norte",
         salas: [
-            "BANHEIRO",
-            "B1",
-            "B2",
-            "AUD."
+            {
+                nome: "BANHEIRO",
+                status: "INTERDITADO",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "B1",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "B2",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "AUD.",
+                status: "OK",
+                role: "GERAL",
+                tipo: "AUD"
+            },
         ]
     },
     {
         bloco: "C",
         sentido: "sul",
         salas: [
-            "C1",
-            "C2",
-            "C3",
-            "C4",
-            "C5",
-            "LAB. 1"
+            {
+                nome: "C1",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "C2",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "C3",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "C4",
+                status: "PENDENTE",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "C5",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "LAB. 1",
+                status: "OK",
+                role: "GERAL",
+                tipo: "LAB"
+            },
         ]
     },
     {
         bloco: "D",
         sentido: "norte",
         salas: [
-            "BANHEIRO",
-            "D1",
-            "D2",
-            "D3",
-            "LAB. 2",
-            "D4"
+            {
+                nome: "BANHEIRO",
+                status: "INTERDITADO",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "D1",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "D2",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "D3",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "LAB. 2",
+                status: "OK",
+                role: "GERAL",
+                tipo: "LAB"
+            },
+            {
+                nome: "D4",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
         ]
     },
     {
         bloco: "E",
         sentido: "sul",
         salas: [
-            "SUPERVISÃO",
-            "SUPORTE",
-            "LAB. 3",
-            "SL. EST.",
-            "LAB. 4"
+            {
+                nome: "SUPERVISÃO",
+                status: "OK",
+                role: "GERAL",
+                tipo: "OUTRO"
+            },
+            {
+                nome: "SUPORTE",
+                status: "OK",
+                role: "GERAL",
+                tipo: "OUTRO"
+            },
+            {
+                nome: "LAB. 3",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "SL. EST.",
+                status: "INTERDITADO",
+                role: "GERAL",
+                tipo: "OUTRO"
+            },
+            {
+                nome: "LAB. 4",
+                status: "OK",
+                role: "GERAL",
+                tipo: "LAB"
+            },
         ]
     },
     {
         bloco: "F",
         sentido: "norte",
         salas: [
-            "BANHEIRO",
-            "F1",
-            "F2",
-            "F3",
-            "F4",
-            "F5"
+            {
+                nome: "BANHEIRO",
+                status: "INTERDITADO",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "F1",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "F2",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "F3",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "F4",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "F5",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
         ]
     },
     {
         bloco: "G",
         sentido: "sul",
         salas: [
-            "LAB. 5",
-            "G1",
-            "G2",
-            "G3",
-            "G4",
-            "LAB. 6"
+            {
+                nome: "LAB. 5",
+                status: "OK",
+                role: "GERAL",
+                tipo: "LAB"
+            },
+            {
+                nome: "G1",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "G2",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "G3",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "G4",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "LAB. 6",
+                status: "OK",
+                role: "GERAL",
+                tipo: "LAB"
+            },
         ]
     },
     {
         bloco: "H",
         sentido: "norte",
         salas: [
-            "BANHEIRO",
-            "H1",
-            "H2",
-            "H3",
-            "H4",
+            {
+                nome: "BANHEIRO",
+                status: "INTERDITADO",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "H1",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "H2",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "H3",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "H4",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
         ]
     },
     {
         bloco: "I",
         sentido: "sul",
         salas: [
-            "ADMIN",
-            "I1",
-            "I2",
-            "I3",
-            "I4",
+            {
+                nome: "ADMIN",
+                status: "ANDAMENTO",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I1",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I2",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I3",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I4",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
             
             
         ]
@@ -134,18 +409,67 @@ const mapa : Bloco[] = [
         bloco: "I",
         sentido: "norte",
         salas: [
-            "BANHEIRO"
+            {
+                nome: "BANHEIRO",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            
         ]
     },
     {
         bloco: "I",
         sentido: "sul",
         salas: [
-            "I5",
-            "I6",
-            "I7",
-            "I8",
-            "I9",
+            {
+                nome: "I5",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I6",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I7",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I8",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I9",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
             
         ]
     },
@@ -153,64 +477,174 @@ const mapa : Bloco[] = [
         bloco: "I",
         sentido: "norte",
         salas: [
-            "BANHEIRO"
+            {
+                nome: "BANHEIRO",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
         ]
     },
     {
         bloco: "I",
         sentido: "sul",
         salas: [
-            "I10",
-            "I11",
-            "I12",
-            "I13",
-            "I14",
-            
+            {
+                nome: "I10",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I11",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I12",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I13",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I14",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
         ]
     },
     {
         bloco: "I",
         sentido: "norte",
         salas: [
-            "BANHEIRO"
+            {
+                nome: "BANHEIRO",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
+            {
+                nome: "",
+                status: "OK",
+                role: "ADMIN",
+                tipo: "BANHEIRO"
+            },
         ]
     },
     {
         bloco: "I",
         sentido: "sul",
         salas: [
-            "I15",
-            "I16",
-            "I17",
-            "I18",
-            "I19"
+            {
+                nome: "I15",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I16",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I17",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I18",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+            {
+                nome: "I19",
+                status: "OK",
+                role: "GERAL",
+                tipo: "SALA"
+            },
+
         ]
     },
 ]
 
+mapa = mapa.map(bloco => ({
+    ...bloco,
+    salas: role_user as string === "ADMIN"
+        ? bloco.salas 
+        : bloco.salas.filter(sala => sala.role !== "ADMIN") 
+}));
+
+
 function Mapa(){
+
+
+
     return(
-        <div className="w-11/12 h-11/12 border-2 p-5 flex flex-col">
+        <div className="w-4/5 h-3/4 border-2 p-5 flex flex-col bg-neutral-100">
             {mapa.map((bloco, index)=>(
                 <div key={index} className="flex flex-col w-full"
                 style={{
                     height: `50px`
                 }}
                 >
-                    
-                    <div className={`w-[49%] h-full ${bloco.sentido === "norte" ? "self-end " : "self-start"}`}
+                    <div className={`w-[45%] h-full ${bloco.sentido === "norte" ? "self-end " : "self-start"}`}
 
                     >
 
                         <div className={`flex h-full w-full ${bloco.sentido === "norte" ? "flex-row" : "flex-row-reverse"} gap-1`}>
+                            <h1 className={`text-sm self-center `}>{bloco.salas.length === 0 ? "" : bloco.bloco}</h1>
                             {mapa[index].salas.map((sala, index)=>(
-                                <div key={index} className={`h-full border-2 text-sm flex justify-center items-center cursor-pointer hover:bg-neutral-100 transition-all duration-300 hover:animate-bounce`}
-                                    style={{ 
-                                        width: `${100 / bloco.salas.length}%` 
-                                    }}
-                                >
-                                    {sala}
-                                </div>
+
+                                 <div key={index} className={`h-full border-2 text-sm flex justify-center items-center cursor-pointer transition-all duration-300 ${sala.status === "PENDENTE" ? "bg-red-500 animate-bounce hover:bg-red-600": ""} ${sala.status === "ANDAMENTO" ? "bg-yellow-300 animate-pulse hover:bg-yellow-300": ""} ${sala.status === "OK" ? "bg-blue-300 hover:bg-blue-400": ""} ${sala.status === "INTERDITADO" ? " bg-blue-300  text-black hover:bg-neutral-100": ""} gap-1 ${sala.nome.length === 0 ? "bg-neutral-100 border-none hover:bg-neutral-100 cursor-" : ""}`}
+                                     style={{ 
+                                         width: `${100 / bloco.salas.length}%` 
+                                     }}
+                                 >
+                                 {sala.status === "INTERDITADO" && <OctagonX className="text-red-700"/>}
+                                 {sala.nome}
+                                 </div>
+
                             ))}
                         </div>
 
